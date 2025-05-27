d24f2ff3eb96b88637eaaeb1f49d79632a2d1de2
karenayman <karenayman98@gmail.com>
2025-05-27 16:32:04 +0300

    route and view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //resources --> views
+});
+//about page route example
+Route::get('/about', function () {
+    return view('about');
+    //return 'about page';
+});
+Route::get('/contact', function () {
+    return view('contact');
+   
 });
