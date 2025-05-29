f86ff0a125aeb185baa32b46d00e365f2616ee2e
karenayman <karenayman98@gmail.com>
2025-05-30 01:39:29 +0300

    active nav

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -26,9 +26,9 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-              <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-              <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+              <x-nav-links href="/" :active="request()->is('/')">Home</x-nav-links>
+              <x-nav-links href="/about" :active="request()->is('about')">About</x-nav-links>
+              <x-nav-links href="/contact" :active="request()->is('contact')">Contact</x-nav-links>
             </div>
           </div>
         </div>
